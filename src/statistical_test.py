ae96734436ad319c1478bdf4925b845a3c1bac80
G-Sommani <giacomo.sommani@gmail.com>
2023-11-30 14:39:50 +0100

    Implemented the test statistic

--- src/statistical_test.py
+++ src/statistical_test.py
@@ -346,4 +346,86 @@ def unc_contribute(sigma1, sigma2):
     return -2 * np.log(sigma1 * sigma2)
 
 
-print(flux_contribute(0.001, -2))
+def angular_dist_score(az_true, zen_true, az_pred, zen_pred):
+    """
+    calculate the MAE of the angular distance between two directions.
+    The two vectors are first converted to cartesian unit vectors,
+    and then their scalar product is computed, which is equal to
+    the cosine of the angle between the two vectors. The inverse
+    cosine (arccos) thereof is then the angle between the two input vectors
+
+    Parameters:
+    -----------
+
+    az_true : float (or array thereof)
+        true azimuth value(s) in radian
+    zen_true : float (or array thereof)
+        true zenith value(s) in radian
+    az_pred : float (or array thereof)
+        predicted azimuth value(s) in radian
+    zen_pred : float (or array thereof)
+        predicted zenith value(s) in radian
+
+    Returns:
+    --------
+
+    dist : float
+        mean over the angular distance(s) in radian
+    """
+    if not (
+        np.all(np.isfinite(az_true))
+        and np.all(np.isfinite(zen_true))
+        and np.all(np.isfinite(az_pred))
+        and np.all(np.isfinite(zen_pred))
+    ):
+        raise ValueError("All arguments must be finite")
+    # pre-compute all sine and cosine values
+    sa1 = np.sin(az_true)
+    ca1 = np.cos(az_true)
+    sz1 = np.sin(zen_true)
+    cz1 = np.cos(zen_true)
+    sa2 = np.sin(az_pred)
+    ca2 = np.cos(az_pred)
+    sz2 = np.sin(zen_pred)
+    cz2 = np.cos(zen_pred)
+    # scalar product of the two cartesian vectors (x = sz*ca, y = sz*sa, z = cz)
+    scalar_prod = sz1 * sz2 * (ca1 * ca2 + sa1 * sa2) + (cz1 * cz2)
+    # scalar product of two unit vectors is always between -1 and 1, this is against nummerical instability
+    # that might otherwise occure from the finite precision of the sine and cosine functions
+    scalar_prod = np.clip(scalar_prod, -1, 1)
+    # convert back to an angle (in radian)
+    return np.average(np.abs(np.arccos(scalar_prod)))
+
+
+def dir_contribute(ra1, dec1, ra2, dec2, raS, decS, sigma1, sigma2):  # in radiants
+    """
+    Contribute to the test statistic related to
+    how near are the alerts to the source. All
+    directions must be given in radiants
+    """
+    phi1 = angular_dist_score(ra1, dec1 + np.pi / 2.0, raS, decS + np.pi / 2.0)
+    phi2 = angular_dist_score(ra2, dec2 + np.pi / 2.0, raS, decS + np.pi / 2.0)
+    cont = -0.5 * ((phi1 / sigma1) ** 2 + (phi2 / sigma2) ** 2)
+    return cont
+
+
+def noise_contribute(dec1, dec2):
+    """
+    Contribute to the test statistic related to
+    the null hypothesis
+    """
+    return -np.log(np.cos(dec1) * np.cos(dec2))
+
+
+def test_statistic(ra1, dec1, sigma1, ra2, dec2, sigma2, raS, decS, z):
+    """
+    Test statistic related to two alerts and a source
+    """
+    c1 = flux_contribute(z, decS)
+    c2 = unc_contribute(sigma1, sigma2)
+    c3 = dir_contribute(ra1, dec1, ra2, dec2, raS, decS, sigma1, sigma2)
+    c4 = noise_contribute(dec1, dec2)
+    ts = c1 + c2 + c3 + c4
+    return ts
+
+
