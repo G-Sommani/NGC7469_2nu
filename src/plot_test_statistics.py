153f2d67df79d66b7120f5a52d168cdc843706f8
G-Sommani <giacomo.sommani@gmail.com>
2024-04-24 13:54:27 +0200

    config file applied to plot_test_statistics.py

--- src/plot_test_statistics.py
+++ src/plot_test_statistics.py
@@ -2,33 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from pathlib import Path
-
-SPLINEMPE = "splinempe"
-MILLIPEDE = "millipede"
-TURIN = "turin"
-MILLIQUAS = "milliquas"
-REDSHIFT = "redshift"
-XRAY = "xray"
-TS_FILENAME = "test_statistic"
-FIGSIZE = (5, 3.5)
-TURIN_LABEL = "Background distrubution,\nTurin catalog"
-MILLIQUAS_LABEL = "Background distrubution,\nMilliquas catalog"
-HISTTYPE = "step"
-NBINS = 50
-LINEWIDTH = 2
-AXVCOLOR = "red"
-AXVLINESTYLE = "--"
-AXVLABEL = "IC220424A & IC230416A\nwith NGC 7469"
-XLABEL = "Negative test statistic"
-YLABEL = "N Datasets"
-TITLE = "Goodness of Fit test"
-XRAYTITLE = "Goodness of Fit test, X-ray weighting"
-MILLIPEDETITLE = "Goodness of Fit test, Millipede"
-MILLIPEDEXRAYTITLE = "Goodness of Fit test, Millipede, X-ray weighting"
-FONTSIZE = "large"
-AXISSCALE = "log"
-FIGNAME = "TS_distr"
-BBOX_INCHES = "tight"
+import config as cfg
 
 print("Definition of paths...")
 
@@ -38,166 +12,200 @@ figures_path = cwd / "../figures"
 
 print("Loading results...")
 
-ts_spl_tur_z = np.load(data_results_path / f"{TS_FILENAME}_{SPLINEMPE}_{TURIN}_{REDSHIFT}.npy")
-ts_spl_tur_z_result = np.load(data_results_path / f"{TS_FILENAME}_{SPLINEMPE}_{TURIN}_{REDSHIFT}_result.npy")
+ts_spl_tur_z = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.SPLINEMPE}_{cfg.TURIN}_{cfg.REDSHIFT}.npy"
+)
+ts_spl_tur_z_result = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.SPLINEMPE}_{cfg.TURIN}_{cfg.REDSHIFT}_result.npy"
+)
 
-ts_spl_mil_z = np.load(data_results_path / f"{TS_FILENAME}_{SPLINEMPE}_{MILLIQUAS}_{REDSHIFT}.npy")
-ts_spl_mil_z_result = np.load(data_results_path / f"{TS_FILENAME}_{SPLINEMPE}_{MILLIQUAS}_{REDSHIFT}_result.npy")
+ts_spl_mil_z = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.SPLINEMPE}_{cfg.MILLIQUAS}_{cfg.REDSHIFT}.npy"
+)
+ts_spl_mil_z_result = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.SPLINEMPE}_{cfg.MILLIQUAS}_{cfg.REDSHIFT}_result.npy"
+)
 
-ts_spl_tur_xray = np.load(data_results_path / f"{TS_FILENAME}_{SPLINEMPE}_{TURIN}_{XRAY}.npy")
-ts_spl_tur_xray_result = np.load(data_results_path / f"{TS_FILENAME}_{SPLINEMPE}_{TURIN}_{XRAY}_result.npy")
+ts_spl_tur_xray = np.load(
+    data_results_path / f"{cfg.TS_FILENAME}_{cfg.SPLINEMPE}_{cfg.TURIN}_{cfg.XRAY}.npy"
+)
+ts_spl_tur_xray_result = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.SPLINEMPE}_{cfg.TURIN}_{cfg.XRAY}_result.npy"
+)
 
-ts_mil_tur_z = np.load(data_results_path / f"{TS_FILENAME}_{MILLIPEDE}_{TURIN}_{REDSHIFT}.npy")
-ts_mil_tur_z_result = np.load(data_results_path / f"{TS_FILENAME}_{MILLIPEDE}_{TURIN}_{REDSHIFT}_result.npy")
+ts_mil_tur_z = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.MILLIPEDE}_{cfg.TURIN}_{cfg.REDSHIFT}.npy"
+)
+ts_mil_tur_z_result = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.MILLIPEDE}_{cfg.TURIN}_{cfg.REDSHIFT}_result.npy"
+)
 
-ts_mil_mil_z = np.load(data_results_path / f"{TS_FILENAME}_{MILLIPEDE}_{MILLIQUAS}_{REDSHIFT}.npy")
-ts_mil_mil_z_result = np.load(data_results_path / f"{TS_FILENAME}_{MILLIPEDE}_{MILLIQUAS}_{REDSHIFT}_result.npy")
+ts_mil_mil_z = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.MILLIPEDE}_{cfg.MILLIQUAS}_{cfg.REDSHIFT}.npy"
+)
+ts_mil_mil_z_result = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.MILLIPEDE}_{cfg.MILLIQUAS}_{cfg.REDSHIFT}_result.npy"
+)
 
-ts_mil_tur_xray = np.load(data_results_path / f"{TS_FILENAME}_{MILLIPEDE}_{TURIN}_{XRAY}.npy")
-ts_mil_tur_xray_result = np.load(data_results_path / f"{TS_FILENAME}_{MILLIPEDE}_{TURIN}_{XRAY}_result.npy")
+ts_mil_tur_xray = np.load(
+    data_results_path / f"{cfg.TS_FILENAME}_{cfg.MILLIPEDE}_{cfg.TURIN}_{cfg.XRAY}.npy"
+)
+ts_mil_tur_xray_result = np.load(
+    data_results_path
+    / f"{cfg.TS_FILENAME}_{cfg.MILLIPEDE}_{cfg.TURIN}_{cfg.XRAY}_result.npy"
+)
 
-print(f"Plotting {SPLINEMPE} with {REDSHIFT}...")
+print(f"Plotting {cfg.SPLINEMPE} with {cfg.REDSHIFT}...")
 
-plt.subplots(figsize=FIGSIZE)
-logbins = np.logspace(0, np.log10(3e3), NBINS)
+plt.subplots(figsize=cfg.FIGSIZE_TS)
+logbins = np.logspace(0, np.log10(3e3), cfg.NBINS_TS)
 plt.hist(
     -ts_spl_tur_z,
-    histtype=HISTTYPE,
+    histtype=cfg.HISTTYPE_TS,
     bins=logbins,
-    linewidth = LINEWIDTH,
-    label= TURIN_LABEL
+    linewidth=cfg.LINEWIDTH_TS,
+    label=cfg.TURIN_LABEL_TS,
 )
 plt.hist(
     -ts_spl_mil_z,
-    histtype=HISTTYPE,
+    histtype=cfg.HISTTYPE_TS,
     bins=logbins,
-    linewidth = LINEWIDTH,
-    label= MILLIQUAS_LABEL
+    linewidth=cfg.LINEWIDTH_TS,
+    label=cfg.MILLIQUAS_LABEL_TS,
 )
 plt.axvline(
     -ts_spl_tur_z_result,
-    color=AXVCOLOR,
-    linestyle=AXVLINESTYLE,
-    label=AXVLABEL
+    color=cfg.AXVCOLOR_TS,
+    linestyle=cfg.AXVLINESTYLE_TS,
+    label=cfg.AXVLABEL_TS,
 )
 plt.gca().invert_xaxis()
-plt.xlabel(XLABEL, fontsize=FONTSIZE)
-plt.ylabel(YLABEL, fontsize=FONTSIZE)
-plt.title(TITLE, fontsize=FONTSIZE)
-plt.xscale(AXISSCALE)
-plt.yscale(AXISSCALE)
+plt.xlabel(cfg.XLABEL_TS, fontsize=cfg.FONTSIZE_TS)
+plt.ylabel(cfg.YLABEL_TS, fontsize=cfg.FONTSIZE_TS)
+plt.title(cfg.TITLE_TS, fontsize=cfg.FONTSIZE_TS)
+plt.xscale(cfg.AXISSCALE_TS)
+plt.yscale(cfg.AXISSCALE_TS)
 plt.legend()
 
 print("Saving plot...")
 
-figname = f"{FIGNAME}_{SPLINEMPE}_{REDSHIFT}"
-plt.savefig(figures_path / figname, bbox_inches = BBOX_INCHES)
+figname = f"{cfg.FIGNAME_TS}_{cfg.SPLINEMPE}_{cfg.REDSHIFT}"
+plt.savefig(figures_path / figname, bbox_inches=cfg.BBOX_INCHES)
 figname_pdf = f"{figname}.pdf"
-plt.savefig(figures_path / figname_pdf, bbox_inches = BBOX_INCHES)
+plt.savefig(figures_path / figname_pdf, bbox_inches=cfg.BBOX_INCHES)
 plt.close()
 
-print(f"Plotting {SPLINEMPE} with {XRAY}...")
+print(f"Plotting {cfg.SPLINEMPE} with {cfg.XRAY}...")
 
-plt.subplots(figsize=FIGSIZE)
-logbins = np.logspace(0, np.log10(2e3), NBINS)
+plt.subplots(figsize=cfg.FIGSIZE_TS)
+logbins = np.logspace(0, np.log10(2e3), cfg.NBINS_TS)
 plt.hist(
-    -ts_spl_tur_xray-np.min(-ts_spl_tur_xray)+1,
-    histtype=HISTTYPE,
+    -ts_spl_tur_xray - np.min(-ts_spl_tur_xray) + 1,
+    histtype=cfg.HISTTYPE_TS,
     bins=logbins,
-    linewidth = LINEWIDTH,
-    label= TURIN_LABEL
+    linewidth=cfg.LINEWIDTH_TS,
+    label=cfg.TURIN_LABEL_TS,
 )
 plt.axvline(
-    -ts_spl_tur_xray_result-np.min(-ts_spl_tur_xray)+1,
-    color=AXVCOLOR,
-    linestyle=AXVLINESTYLE,
-    label=AXVLABEL
+    -ts_spl_tur_xray_result - np.min(-ts_spl_tur_xray) + 1,
+    color=cfg.AXVCOLOR_TS,
+    linestyle=cfg.AXVLINESTYLE_TS,
+    label=cfg.AXVLABEL_TS,
 )
 plt.gca().invert_xaxis()
-plt.xlabel(XLABEL, fontsize=FONTSIZE)
-plt.ylabel(YLABEL, fontsize=FONTSIZE)
-plt.title(XRAYTITLE, fontsize=FONTSIZE)
-plt.xscale(AXISSCALE)
-plt.yscale(AXISSCALE)
+plt.xlabel(cfg.XLABEL_TS, fontsize=cfg.FONTSIZE_TS)
+plt.ylabel(cfg.YLABEL_TS, fontsize=cfg.FONTSIZE_TS)
+plt.title(cfg.XRAYTITLE_TS, fontsize=cfg.FONTSIZE_TS)
+plt.xscale(cfg.AXISSCALE_TS)
+plt.yscale(cfg.AXISSCALE_TS)
 plt.legend()
 
 print("Saving plot...")
 
-figname = f"{FIGNAME}_{SPLINEMPE}_{XRAY}"
-plt.savefig(figures_path / figname, bbox_inches = BBOX_INCHES)
+figname = f"{cfg.FIGNAME_TS}_{cfg.SPLINEMPE}_{cfg.XRAY}"
+plt.savefig(figures_path / figname, bbox_inches=cfg.BBOX_INCHES)
 figname_pdf = f"{figname}.pdf"
-plt.savefig(figures_path / figname_pdf, bbox_inches = BBOX_INCHES)
+plt.savefig(figures_path / figname_pdf, bbox_inches=cfg.BBOX_INCHES)
 plt.close()
 
-print(f"Plotting {MILLIPEDE} with {REDSHIFT}...")
+print(f"Plotting {cfg.MILLIPEDE} with {cfg.REDSHIFT}...")
 
-plt.subplots(figsize=FIGSIZE)
-logbins = np.logspace(0, np.log10(60), NBINS)
+plt.subplots(figsize=cfg.FIGSIZE_TS)
+logbins = np.logspace(0, np.log10(60), cfg.NBINS_TS)
 plt.hist(
     -ts_mil_tur_z,
-    histtype=HISTTYPE,
+    histtype=cfg.HISTTYPE_TS,
     bins=logbins,
-    linewidth = LINEWIDTH,
-    label= TURIN_LABEL
+    linewidth=cfg.LINEWIDTH_TS,
+    label=cfg.TURIN_LABEL_TS,
 )
 plt.hist(
     -ts_mil_mil_z,
-    histtype=HISTTYPE,
+    histtype=cfg.HISTTYPE_TS,
     bins=logbins,
-    linewidth = LINEWIDTH,
-    label= MILLIQUAS_LABEL
+    linewidth=cfg.LINEWIDTH_TS,
+    label=cfg.MILLIQUAS_LABEL_TS,
 )
 plt.axvline(
     -ts_mil_tur_z_result,
-    color=AXVCOLOR,
-    linestyle=AXVLINESTYLE,
-    label=AXVLABEL
+    color=cfg.AXVCOLOR_TS,
+    linestyle=cfg.AXVLINESTYLE_TS,
+    label=cfg.AXVLABEL_TS,
 )
 plt.gca().invert_xaxis()
-plt.xlabel(XLABEL, fontsize=FONTSIZE)
-plt.ylabel(YLABEL, fontsize=FONTSIZE)
-plt.title(MILLIPEDETITLE, fontsize=FONTSIZE)
-plt.xscale(AXISSCALE)
-plt.yscale(AXISSCALE)
+plt.xlabel(cfg.XLABEL_TS, fontsize=cfg.FONTSIZE_TS)
+plt.ylabel(cfg.YLABEL_TS, fontsize=cfg.FONTSIZE_TS)
+plt.title(cfg.MILLIPEDETITLE_TS, fontsize=cfg.FONTSIZE_TS)
+plt.xscale(cfg.AXISSCALE_TS)
+plt.yscale(cfg.AXISSCALE_TS)
 plt.legend()
 
 print("Saving plot...")
 
-figname = f"{FIGNAME}_{MILLIPEDE}_{REDSHIFT}"
-plt.savefig(figures_path / figname, bbox_inches = BBOX_INCHES)
+figname = f"{cfg.FIGNAME_TS}_{cfg.MILLIPEDE}_{cfg.REDSHIFT}"
+plt.savefig(figures_path / figname, bbox_inches=cfg.BBOX_INCHES)
 figname_pdf = f"{figname}.pdf"
-plt.savefig(figures_path / figname_pdf, bbox_inches = BBOX_INCHES)
+plt.savefig(figures_path / figname_pdf, bbox_inches=cfg.BBOX_INCHES)
 plt.close()
 
-print(f"Plotting {MILLIPEDE} with {XRAY}...")
+print(f"Plotting {cfg.MILLIPEDE} with {cfg.XRAY}...")
 
-plt.subplots(figsize=FIGSIZE)
-logbins = np.logspace(0, np.log10(2e3), NBINS)
+plt.subplots(figsize=cfg.FIGSIZE_TS)
+logbins = np.logspace(0, np.log10(2e3), cfg.NBINS_TS)
 plt.hist(
-    -ts_mil_tur_xray-np.min(-ts_mil_tur_xray)+1,
-    histtype=HISTTYPE,
+    -ts_mil_tur_xray - np.min(-ts_mil_tur_xray) + 1,
+    histtype=cfg.HISTTYPE_TS,
     bins=logbins,
-    linewidth = LINEWIDTH,
-    label= TURIN_LABEL
+    linewidth=cfg.LINEWIDTH_TS,
+    label=cfg.TURIN_LABEL_TS,
 )
 plt.axvline(
-    -ts_mil_tur_xray_result-np.min(-ts_mil_tur_xray)+1,
-    color=AXVCOLOR,
-    linestyle=AXVLINESTYLE,
-    label=AXVLABEL
+    -ts_mil_tur_xray_result - np.min(-ts_mil_tur_xray) + 1,
+    color=cfg.AXVCOLOR_TS,
+    linestyle=cfg.AXVLINESTYLE_TS,
+    label=cfg.AXVLABEL_TS,
 )
 plt.gca().invert_xaxis()
-plt.xlabel(XLABEL, fontsize=FONTSIZE)
-plt.ylabel(YLABEL, fontsize=FONTSIZE)
-plt.title(MILLIPEDEXRAYTITLE, fontsize=FONTSIZE)
-plt.xscale(AXISSCALE)
-plt.yscale(AXISSCALE)
+plt.xlabel(cfg.XLABEL_TS, fontsize=cfg.FONTSIZE_TS)
+plt.ylabel(cfg.YLABEL_TS, fontsize=cfg.FONTSIZE_TS)
+plt.title(cfg.MILLIPEDEXRAYTITLE_TS, fontsize=cfg.FONTSIZE_TS)
+plt.xscale(cfg.AXISSCALE_TS)
+plt.yscale(cfg.AXISSCALE_TS)
 plt.legend()
 
 print("Saving plot...")
 
-figname = f"{FIGNAME}_{MILLIPEDE}_{XRAY}"
-plt.savefig(figures_path / figname, bbox_inches = BBOX_INCHES)
+figname = f"{cfg.FIGNAME_TS}_{cfg.MILLIPEDE}_{cfg.XRAY}"
+plt.savefig(figures_path / figname, bbox_inches=cfg.BBOX_INCHES)
 figname_pdf = f"{figname}.pdf"
-plt.savefig(figures_path / figname_pdf, bbox_inches = BBOX_INCHES)
+plt.savefig(figures_path / figname_pdf, bbox_inches=cfg.BBOX_INCHES)
 plt.close()
