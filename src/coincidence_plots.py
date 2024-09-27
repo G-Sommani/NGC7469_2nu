9e26f44dfa9a84b2d8bf0da265373789ec3e82c8
G-Sommani <giacomo.sommani@gmail.com>
2024-09-27 18:25:29 +0200

    merged plots of GCN Notices and Circulars

--- src/coincidence_plots.py
+++ src/coincidence_plots.py
@@ -168,3 +168,91 @@ plt.savefig(figures_path / cfg.NOTICE_PLOT_FILENAME, bbox_inches="tight")
 notice_plot_filename_pdf = cfg.NOTICE_PLOT_FILENAME + ".pdf"
 plt.savefig(figures_path / notice_plot_filename_pdf, bbox_inches="tight")
 plt.close()
+
+print("Plotting GCN Circulars and GCN Notices together...")
+
+fig, ax = plt.subplots(figsize=(5, 5))
+
+plt.plot(
+    ra_vertices_22,
+    de_vertices_22,
+    color="tab:blue",
+    linestyle="--",
+    label="IC220424A Circular",
+    linewidth=2,
+)
+plt.plot(
+    ras_err_22,
+    des_err_22,
+    color="tab:blue",
+    #linestyle="dotted",
+    label="IC220424A Notice",
+    linewidth=2,
+)
+plt.plot(
+    ra_vertices_23,
+    de_vertices_23,
+    color="tab:orange",
+    linestyle="--",
+    label="IC230416A Circular",
+    linewidth=2,
+)
+plt.plot(
+    ras_err_23,
+    des_err_23,
+    color="tab:orange",
+    #linestyle="dotted",
+    label="IC230416A Notice",
+    linewidth=2,
+)
+#plt.plot(
+#    cfg.CIRCULAR_22_COORDS[cfg.RA_INDEX],
+#    cfg.CIRCULAR_22_COORDS[cfg.DE_INDEX],
+#    marker="o",
+#    color="tab:blue",
+#)
+#plt.plot(
+#    cfg.CIRCULAR_23_COORDS[cfg.RA_INDEX],
+#    cfg.CIRCULAR_23_COORDS[cfg.DE_INDEX],
+#    color="tab:orange",
+#    marker="o",
+#)
+#plt.plot(
+#    cfg.NOTICE_22_COORDS[cfg.RA_INDEX],
+#    cfg.NOTICE_22_COORDS[cfg.DE_INDEX],
+#    marker="s",
+#    color="tab:blue",
+#)
+#plt.plot(
+#    cfg.NOTICE_23_COORDS[cfg.RA_INDEX],
+#    cfg.NOTICE_23_COORDS[cfg.DE_INDEX],
+#    color="tab:orange",
+#    marker="s",
+#)
+plt.plot(
+    cfg.NGC7469_COORDS[cfg.RA_INDEX],
+    cfg.NGC7469_COORDS[cfg.DE_INDEX],
+    marker="*",
+    markeredgecolor="black",
+    color="yellow",
+    markersize=20,
+    label="NGC 7469",
+    linestyle="",
+    markeredgewidth=1.5,
+)
+plt.xlabel("Right Ascension [$^\circ$]", size="x-large")
+plt.ylabel("Declination [$^\circ$]", size="x-large")
+plt.xlim(344.5, 348)
+plt.ylim(7.8, 11.5)
+plt.title("Circulars' and Notices' 90% contours", size="xx-large")
+plt.legend(fontsize="large")
+ax.set_xticks([345, 346, 347, 348])
+ax.set_xticklabels([345, 346, 347, 348], size="x-large")
+ax.set_yticklabels(ax.get_yticks(), size="x-large")
+
+print("Saving GCN Circulars and GCN Notices plot...")
+
+plt.savefig(figures_path / cfg.CIRCULAR_NOTICE_PLOT_FILENAME, bbox_inches="tight")
+circular_notice_plot_filename_pdf = cfg.CIRCULAR_NOTICE_PLOT_FILENAME + ".pdf"
+plt.savefig(figures_path / circular_notice_plot_filename_pdf, bbox_inches="tight")
+plt.close()
